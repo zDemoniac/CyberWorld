00eaed654678c4efd93cbbca6297004762c2b86a
zdemoniac <zdemoniac@gmail.com>
2013-06-25 16:24:09 +0300

    b

---
+++ js/units.js
@@ -0,0 +1,30 @@
+
+function Unit0(scene,loc,loader) {
+    this.rotSpeed = 1.0;
+    this.speed = 2.5;
+    this.closeEnough = 0.1;
+    this.goal = loc;
+    this.dx = new THREE.Vector3();
+    var that = this;
+    this.onGeometry = function(geom, mats) {
+//        that.mesh = new THREE.Mesh( geom, new THREE.MeshFaceMaterial(mats));
+        that.mesh.useQuaternion = true;
+        that.mesh.position = loc;
+        that.mesh.name = "Unit";
+        scene.add(that.mesh);
+    };
+//    loader.load( "models/unit0.js", onGeometry );
+    this.mesh = new THREE.Mesh( new THREE.CubeGeometry( 1, 1, 1 ), new THREE.MeshBasicMaterial( { color: 0x00ff00 } ) );
+    this.onGeometry(null, null);
+
+    this.prerender = function(dt) {
+        if (!this.mesh)  return;
+        var dTheta = dt * this.rotSpeed;
+        lookTowards(this.mesh, this.goal, dTheta);
+        this.dx.subVectors(this.goal, this.mesh.position);
+        if (this.dx.length() > this.closeEnough) {
+            var moveDist = dt * this.speed;
+            this.mesh.translateZ(moveDist);
+        }
+    };
+}
